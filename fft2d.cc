08d9540cdcc89f8e27d92b89a1c9986820534c2f
Mohit Prabhushankar <Mohit@lawn-128-61-68-180.lawn.gatech.edu>
2015-09-08 18:53:37 -0400

    Commit code

--- fft2d.cc
+++ fft2d.cc
@@ -10,13 +10,18 @@
 #include <vector>
 #include <signal.h>
 #include <math.h>
-#include <mpi.h>
+#include <complex>
+//#include <mpi.h>
 
 #include "Complex.h"
 #include "InputImage.h"
 
 using namespace std;
 
+typedef complex<double> dcomp;
+
+
+void Transform1D(Complex* h, int w, Complex* H);
 
 void Transform2D(const char* inputFN) 
 { // Do the 2D transform here.
@@ -37,15 +42,51 @@ void Transform2D(const char* inputFN)
   // 9) Send final answers to CPU 0 (unless you are CPU 0)
   //   9a) If you are CPU 0, collect all values from other processors
   //       and print out with SaveImageData().
-  InputImage image(Tower.txt);  // Create the helper object for reading the image
+  InputImage image(inputFN);  // Create the helper object for reading the image
   // Step (1) in the comments is the line above.
   // Your code here, steps 2-9
-
+    
+    
 	int w = image.GetWidth();
 	int height = image.GetHeight();
-		
-	double Complex H = zeros(w*height);
-	
+    
+    Complex* H = new Complex[w*height];
+    Complex* H_Temp = new Complex[w*height];
+    Complex* H_Final = new Complex[w*height];
+    
+    Complex* imagePixels = image.GetImageData();
+    //1D transform of all rows
+    for(int ii=0;ii<height;ii++)
+    {
+        Transform1D(imagePixels+(ii*w),w,H+(ii*w));
+    }
+
+    image.SaveImageData("after1d_mohit.txt", H, w, height);
+
+    //Transpose of the matrix
+    for(int jj=0;jj<w;jj++)
+    {
+        for(int kk=0;kk<height;kk++)
+        {
+            H_Temp[kk,jj] = H[jj,kk];
+        }
+    }
+    //Transform 1D data
+    for(int ll=0;ll<w;ll++)
+    {
+        Transform1D(H_Temp+(ll*height),height,H+(ll*height));
+    }
+    
+    //Transpose of the matrix
+    for(int jj=0;jj<w;jj++)
+    {
+        for(int kk=0;kk<height;kk++)
+        {
+            H_Final[kk,jj] = H[jj,kk];
+        }
+    }
+    
+    image.SaveImageData("after2d_mohit.txt", H, w, height);
 }
 
 void Transform1D(Complex* h, int w, Complex* H)
@@ -53,19 +94,26 @@ void Transform1D(Complex* h, int w, Complex* H)
   // Implement a simple 1-d DFT using the double summation equation
   // given in the assignment handout.  h is the time-domain input
   // data, w is the width (N), and H is the output array.
+    
+//    dcomp i;
+//    
+//    i = -1;
+//    i = sqrt(i);
+    
 	for(int n=0; n<w; n++)
 	{
-		double Complex W;
+		Complex W_DFT;
 		for(int k=0;k<w;k++)
 		{
-			W=pow(exp(I*2*pi/w),(n*k));
-			H[n] = H[n]+ W*h[k];
+			W_DFT=pow(exp(sqrt(-1)*2*M_PI/w),(n*k));
+			H[n] = H[n]+ W_DFT*h[k];
 		}
 	}
 }
 
 int main(int argc, char** argv)
 {
+    
   string fn("Tower.txt"); // default file name
   if (argc > 1) fn = string(argv[1]);  // if name specified on cmd line
   // MPI initialization here
