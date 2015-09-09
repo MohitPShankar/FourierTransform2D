11614fd61706b8569bbccce1084cbc418a911c6b
Mohit Prabhushankar <Mohit@lawn-128-61-68-180.lawn.gatech.edu>
2015-09-09 00:07:14 -0400

    Commit code

--- fft2d.cc
+++ fft2d.cc
@@ -10,19 +10,14 @@
 #include <vector>
 #include <signal.h>
 #include <math.h>
-#include <complex>
-//#include <mpi.h>
+#include <mpi.h>
 
 #include "Complex.h"
 #include "InputImage.h"
 
 using namespace std;
 
-typedef complex<double> dcomp;
-
-
-void Transform1D(Complex* h, int w, Complex* H);
-
+void Transform1D(Complex* h, int w, Complex* H, int bin);
 void Transform2D(const char* inputFN) 
 { // Do the 2D transform here.
   // 1) Use the InputImage object to read in the Tower.txt file and
@@ -45,67 +40,109 @@ void Transform2D(const char* inputFN)
   InputImage image(inputFN);  // Create the helper object for reading the image
   // Step (1) in the comments is the line above.
   // Your code here, steps 2-9
-    
-    
-	int w = image.GetWidth();
-	int height = image.GetHeight();
+    int fwd = 1;
+    int inv = 0;
+    int w = image.GetWidth();
+    int height = image.GetHeight();
     
     Complex* H = new Complex[w*height];
     Complex* H_Temp = new Complex[w*height];
+    Complex* H_Temp2 = new Complex[w*height];
     Complex* H_Final = new Complex[w*height];
+    Complex* H_Temp_inv = new Complex[w*height];
+    Complex* H_Temp2_inv = new Complex[w*height];
+    Complex* H_Temp3_inv = new Complex[w*height];
+    Complex* H_Final_inv = new Complex[w*height];
+
+
     
     Complex* imagePixels = image.GetImageData();
     //1D transform of all rows
     for(int ii=0;ii<height;ii++)
     {
-        Transform1D(imagePixels+(ii*w),w,H+(ii*w));
+        Transform1D(imagePixels+(ii*w),w,H+(ii*w), fwd);
     }
 
-    image.SaveImageData("after1d_mohit.txt", H, w, height);
 
     //Transpose of the matrix
     for(int jj=0;jj<w;jj++)
     {
         for(int kk=0;kk<height;kk++)
         {
-            H_Temp[kk,jj] = H[jj,kk];
+        	H_Temp[kk+height*jj] = H[jj+w*kk];	
         }
     }
+
     //Transform 1D data
     for(int ll=0;ll<w;ll++)
     {
-        Transform1D(H_Temp+(ll*height),height,H+(ll*height));
+        Transform1D(H_Temp+(ll*height),height,H_Temp2+(ll*height),fwd);
     }
-    
+   
     //Transpose of the matrix
     for(int jj=0;jj<w;jj++)
     {
         for(int kk=0;kk<height;kk++)
         {
-            H_Final[kk,jj] = H[jj,kk];
+                  H_Final[jj+w*kk] = H_Temp2[kk+height*jj];
         }
     }
     
-    image.SaveImageData("after2d_mohit.txt", H, w, height);
+    
+    for(int ii=0;ii<w;ii++)
+    {
+     	Transform1D(H_Final+(ii*w),w,H_Temp_inv+(ii*w), inv);
+    }
+
+    //First Transpose
+    for(int jj=0;jj<w;jj++)
+    {
+     	for(int kk=0;kk<height;kk++)
+        {
+                H_Temp2_inv[kk+height*jj] = H_Temp_inv[jj+w*kk];
+        }
+    }
+
+    //Second 1d Inverse DFT
+    for(int ll=0;ll<w;ll++)
+    {
+     	Transform1D(H_Temp2_inv+(ll*height),height,H_Temp3_inv+(ll*height),inv);
+    }
+
+    for(int jj=0;jj<w;jj++)
+    {
+        for(int kk=0;kk<height;kk++)
+        {
+                  H_Final_inv[jj+w*kk] = H_Temp3_inv[kk+height*jj];
+        }
+    }
+
+    image.SaveImageData("MyAfter2d_INV.txt", H_Final_inv, w, height);
+    		
 }
 
-void Transform1D(Complex* h, int w, Complex* H)
+void Transform1D(Complex* h, int w, Complex* H, int bin)
 {
   // Implement a simple 1-d DFT using the double summation equation
   // given in the assignment handout.  h is the time-domain input
   // data, w is the width (N), and H is the output array.
-    
-//    dcomp i;
-//    
-//    i = -1;
-//    i = sqrt(i);
-    
+	
 	for(int n=0; n<w; n++)
 	{
-		Complex W_DFT;
+		Complex W_DFT(0,0);
 		for(int k=0;k<w;k++)
 		{
-			W_DFT=pow(exp(sqrt(-1)*2*M_PI/w),(n*k));
+			double real = cos(2*M_PI*n*k/w);
+      			double imag = sin(2*M_PI*n*k/w);
+			if( bin == 0)
+			{
+				real = real/w;
+				imag = imag/w;
+				Complex W_DFT(real,-imag);
+				H[n] = H[n]+W_DFT*h[k];
+				continue;
+			}
+            Complex W_DFT(real, imag);
 			H[n] = H[n]+ W_DFT*h[k];
 		}
 	}
@@ -113,7 +150,6 @@ void Transform1D(Complex* h, int w, Complex* H)
 
 int main(int argc, char** argv)
 {
-    
   string fn("Tower.txt"); // default file name
   if (argc > 1) fn = string(argv[1]);  // if name specified on cmd line
   // MPI initialization here
